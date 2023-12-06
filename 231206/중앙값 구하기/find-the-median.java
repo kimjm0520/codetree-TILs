6fb963507ca4ae4935dd6a27f150834d716cce16
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-06 23:21:02 +0900

    Title: 중앙값 구하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 231206/중앙값 구하기/find-the-median.java
+++ 231206/중앙값 구하기/find-the-median.java
@@ -7,7 +7,7 @@ public class Main {
         int b=sc.nextInt();
         int c=sc.nextInt();
         if((a>=b && b>=c) || (c>=b && b>=a)){
-            System.out.print(b)
+            System.out.print(b);
         }
         else if((b>=a && a>=c) || (c>=a && a>=b)){
             System.out.print(a);
