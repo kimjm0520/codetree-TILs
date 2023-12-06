e202abea4aea5ac090e590d62e567661d2c20921
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-06 22:30:17 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231206/두 사람/two-person.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int aa = sc.nextInt();
+        char am = sc.next().charAt(0);
+        int ba = sc.nextInt();
+        char bm = sc.next().charAt(0);
+        if((aa>=19 && am=="M") || (ba>=19 && bm=="M")){
+            System.out.print(1);
+        }
+        else{
+            System.out.print(0)
+        }
+
+    }
+}
