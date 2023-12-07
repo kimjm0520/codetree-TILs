7c0400d7d3322e6af39e7687ac973ed44ec722bd
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-07 16:40:32 +0900

    Title: 문자열 출력하기, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231207/문자열 출력하기/print-string.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        String b="LeebrosCode";
+        for(int i=1;i<=a;i++){
+            System.out.println(b);
+        }
+    }
+}
