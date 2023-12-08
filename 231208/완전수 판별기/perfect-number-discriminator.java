3b6eb88af75e2912c0fdc6ac7765424e8a8bbe2f
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-08 14:02:01 +0900

    Title: 완전수 판별기, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 231208/완전수 판별기/perfect-number-discriminator.java
+++ 231208/완전수 판별기/perfect-number-discriminator.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int sum=0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<n;i++){
             if(n%i==0){
                 sum+=i;
             }
