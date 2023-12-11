40dad5c8820accc285973ce3f283704df077ca39
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-11 14:16:29 +0900

    Title: 별표 출력하기 10, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231211/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int even=1,odd=1;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                for(int j=1;j<=odd;j++){
+                    System.out.print("* ");
+                }
+                odd++;
+            }
+            else{
+                for(int j=n;j>=even;j--){
+                    System.out.print("* ");
+                }
+                even++;
+            }
+            System.out.println();
+        }
+        even--;
+         odd--;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                for(int j=1;j<=odd;j++){
+                    System.out.print("* ");
+                }
+                odd--;
+            }
+            else{
+                for(int j=n;j>=even;j--){
+                    System.out.print("* ");
+                }
+                even--;
+            }
+            System.out.println();
+        }
+    }
+}
