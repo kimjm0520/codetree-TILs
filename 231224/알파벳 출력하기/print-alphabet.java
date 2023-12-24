48ab0b5b60a9f922df482fc6459a4e8e4750d049
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2023-12-24 23:31:13 +0900

    Title: 알파벳 출력하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231224/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(), a='A';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(a>90)
+                a='A';
+                System.out.print((char)a);
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
