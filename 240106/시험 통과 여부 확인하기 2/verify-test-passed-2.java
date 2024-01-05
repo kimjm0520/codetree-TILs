9304f039753206aaee562e0234c6f7c5b202e0e5
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-01-06 01:13:40 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240106/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0,cnt=0;
+        for(int i=0;i<n;i++){
+            int[] num=new int[4];
+            sum=0;
+            for(int j=0;j<4;j++){
+                num[i]=sc.nextInt();
+                sum+=num[i];
+            }
+            if((double)sum/4 >= 60){
+                System.out.println("pass");
+                cnt++;
+            }
+            else{
+                System.out.println("fail");
+            }
+        }
+        System.out.print(cnt);
+    }
+}
