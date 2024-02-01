e41ed816cc1f32e455c1a1ab8219445b47d30974
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-02-01 16:09:17 +0900

    Title: 2개씩 그룹짓기 2, Time: 1039ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240201/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240201/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -1,4 +1,9 @@
 n=int(input())
 num=list(map(int,input().split()))
 num.sort()
-print(num[n+1]-num[n-2])
+arr=[]
+for i in range(2*n):
+    for j in range(i+1,2*n):
+        arr.append(num[j]-num[i])
+arr.sort()
+print(arr[len(arr)//2])
