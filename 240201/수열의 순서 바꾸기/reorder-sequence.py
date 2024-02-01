255f5f77de5476f20c06a276ac8237b72e4289e1
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-02-01 14:59:13 +0900

    Title: 수열의 순서 바꾸기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240201/수열의 순서 바꾸기/reorder-sequence.py
@@ -0,0 +1,23 @@
+n=int(input())
+num=list(map(int,input().split()))
+arr=num[:]
+arr.sort()
+cnt=0
+for i in range(n):
+    if arr==num:
+        break
+    tmp=num[0]
+    del num[0]
+    for i in range(1,n-1):
+        if tmp==1:
+            if num[i]==n:
+                num.insert(i+1,tmp)
+                break
+        elif tmp+1==num[i]:
+            num.insert(i,tmp)
+            break
+        elif tmp==n:
+            num.append(tmp)
+            break
+    cnt+=1
+print(cnt)
