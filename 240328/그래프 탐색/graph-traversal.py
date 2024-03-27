52a22943da99ac88e402f9aaff754445b9fbc882
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-03-28 00:44:26 +0900

    Title: 그래프 탐색, Time: 219ms, Memory: 33MB, Status: Passed - CodeTree

--- 240328/그래프 탐색/graph-traversal.py
+++ 240328/그래프 탐색/graph-traversal.py
@@ -1,3 +1,4 @@
+"""
 n,m=map(int,input().split())
 graph=[[] for _ in range(n+1)]
 visited=[False for _ in range(n+1)]
@@ -17,6 +18,30 @@ def dfs(vertex):
             dfs(cur_v)
 
 root_vertex = 1
+visited[root_vertex]=True
+dfs(root_vertex)
+print(v)
+"""
+
+n,m=map(int,input().split())
+graph=[[0 for _ in range(n+1)] for _ in range(n+1)]
+visited=[False for _ in range(n+1)]
+v=0
+
+for _ in range(m):
+    start,end=map(int,input().split())
+    graph[start][end]=1
+    graph[end][start]=1
+
+def dfs(vertex):
+    global v
+    for cur_v in range(1,n+1):
+        if graph[vertex][cur_v] and not visited[cur_v]:
+            v+=1
+            visited[cur_v]=True
+            dfs(cur_v)
+
+root_vertex=1
 visited[root_vertex]=True
 dfs(root_vertex)
 print(v)
