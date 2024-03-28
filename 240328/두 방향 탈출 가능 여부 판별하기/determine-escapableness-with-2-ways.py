1174a3fcc57d9d52f61cf4500d58febe0f853f9e
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-03-28 13:37:40 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240328/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240328/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,40 +1,32 @@
 n,m=map(int,input().split())
-num=[]
+graph=[]
 for _ in range(n):
     a=list(map(int,input().split()))
-    num.append(a)
-visited=[[0 for _ in range(m)] for _ in range(n)]
-p=[]
+    graph.append(a)
+vistied=[[0 for _ in range(m)] for _ in range(n)]
 
-def justice(x,y):
-    if x == (m-1) and y == (n-1):
-        return 1
-    return 0
-
-def pos(x, y):
-    return (0 <= x <= (m-1)) and (0 <= y <= (n-1))
+def ran(x,y):
+    return (0<=x<=(m-1)) and (0<=y<=(n-1))
 
 def can(x,y):
-    if not pos(x,y):
+    if not ran(x,y):
         return False
-    if visited[y][x] or num[y][x] == 0:
+    if vistied[y][x] and (graph[y][x]==0):
         return False
     return True
 
-def grid_dfs(x,y):
-    global p
-    dx=[1,0]
-    dy=[0,1]
+def greid_dfs(x,y):
+    dx, dy=[1,0], [0,1]
     for i in range(2):
         new_x, new_y = x+dx[i], y+dy[i]
+        if can(x,y):
+            vistied[new_y][new_x]=1
+            greid_dfs(new_x, new_y)
+    if x==(m-1) and y==(n-1):
+        return 1
 
-        if can(new_x, new_y):
-            visited[new_y][new_x]=1
-            grid_dfs(new_x,new_y)
-    a=justice(x,y)
-    p.append(a)
-
-visited[0][0]=1
-x,y=0,0
-grid_dfs(x,y)
-print(p[0])
+vistied[0][0]=1
+if greid_dfs(0,0):
+    print(1)
+else:
+    print(0)
