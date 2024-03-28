36241b71ae3c00c43d14ac03a38c2c3bf2da424a
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-03-28 13:40:52 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240328/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,7 +6,7 @@ for _ in range(n):
 vistied=[[0 for _ in range(m)] for _ in range(n)]
 
 def ran(x,y):
-    return (0<=x<=(m-1)) and (0<=y<=(n-1))
+    return (0<=x and x<m) and (0<=y and y<n)
 
 def can(x,y):
     if not ran(x,y):
@@ -19,7 +19,7 @@ def greid_dfs(x,y):
     dx, dy=[1,0], [0,1]
     for i in range(2):
         new_x, new_y = x+dx[i], y+dy[i]
-        if can(x,y):
+        if can(new_x,new_y):
             vistied[new_y][new_x]=1
             greid_dfs(new_x, new_y)
     if x==(m-1) and y==(n-1):
