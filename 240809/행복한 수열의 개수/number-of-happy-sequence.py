75167c387cb791afb8f896bbc1f722d13265083c
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-08-09 05:05:01 +0900

    Title: 행복한 수열의 개수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240809/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240809/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,18 +4,18 @@ graph=[]
 for _ in range(n):
     a=list(map(int,input().split()))
     graph.append(a)
+
+
 for i in range(n):
     cnt=1
     cnt_max=1
     for j in range(1,n):
         if graph[i][j-1]==graph[i][j]:
             cnt+=1
-        else:
-            if cnt_max<cnt:
-                cnt_max=cnt
+        if graph[i][j-1]!=graph[i][j]:
             cnt=1
-    if cnt_max<cnt:
-        cnt_max=cnt
+        if cnt_max<cnt:
+            cnt_max=cnt
     if cnt_max>=m:
         answer+=1
 
@@ -25,12 +25,10 @@ for j in range(n):
     for i in range(1,n):
         if graph[i-1][j]==graph[i][j]:
             cnt+=1
-        else:
-            if cnt_max<cnt:
-                cnt_max=cnt
+        if graph[i-1][j]==graph[i][j]:
             cnt=1
-    if cnt_max<cnt:
-        cnt_max=cnt
+        if cnt_max<cnt:
+            cnt_max=cnt
     if cnt_max>=m:
         answer+=1
 print(answer)
