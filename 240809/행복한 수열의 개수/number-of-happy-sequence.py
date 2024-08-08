7aa2849f81fc07f86ee568794401d62f3ce53b97
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-08-09 04:53:04 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240809/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240809/행복한 수열의 개수/number-of-happy-sequence.py
@@ -9,13 +9,18 @@ for i in range(n):
     for j in range(1,n):
         if graph[i][j-1]==graph[i][j]:
             cnt+=1
+        else:
+            cnt=1
     if cnt>=m:
         answer+=1
+
 for j in range(n):
     cnt=1
     for i in range(1,n):
         if graph[i-1][j]==graph[i][j]:
             cnt+=1
+        else:
+            cnt=1
     if cnt>=m:
         answer+=1
 print(answer)
