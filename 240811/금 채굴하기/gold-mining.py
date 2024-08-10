50219ee355450279ea73ae4681114010c124fa65
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-08-11 08:19:46 +0900

    Title: 금 채굴하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240811/금 채굴하기/gold-mining.py
@@ -0,0 +1,36 @@
+n,m=map(int,input().split())
+graph=[list(map(int,input().split())) for _ in range(n)]
+coin=0
+for i in range(n):
+    coin+=graph[i].count(1)
+
+k=0
+while k**2+k <= (m*coin)//2:
+    k+=1
+k-=1
+
+def can(c1,r1):
+    if c1<0 or c1>=n or r1<0 or r1>=n:
+        return False
+    return True
+
+def run(c1,r1):
+    b=-k
+    cnt=0
+    for i in range(c1-k,c1+k+1):
+        a=abs(b)
+        for j in range(r1-k+a,r1+k+1-a):
+            if can(i,j):
+                cnt+=graph[i][j]
+        b+=1
+    return cnt
+
+cnt_max=0
+
+for i in range(n):
+    for j in range(n):
+        cnt=run(i,j)
+        if cnt_max<cnt:
+            cnt_max=cnt
+
+print(cnt_max)
