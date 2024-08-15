771a6584b07cb01e38de6bbbf541c1fd8df193ff
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-08-15 16:30:07 +0900

    Title: 양수 직사각형의 최대 크기, Time: 156ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240815/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -0,0 +1,31 @@
+n,m=map(int,input().split())
+graph=[list(map(int,input().split())) for _ in range(n)]
+ans=-1
+
+def can(c,r):
+    if c<0 or c>=n or r<0 or r>=m:
+        return False
+    return True
+
+def run(c,r,cnt_max):
+    cnt=0
+    for i in range(c,n):
+        for j in range(r,m):
+            p=False
+            for k in range(c,i+1):
+                if graph[k][j]<1:
+                    p=True
+                    break
+            if p:
+                break
+            cnt=(i-c+1)*(j-r+1)
+            if cnt>cnt_max:
+                cnt_max=cnt
+    return cnt_max
+
+
+cnt_max=-1
+for i in range(n):
+    for j in range(m):
+        cnt_max=run(i,j,cnt_max)
+print(cnt_max)
