3476be81e9ac16843d92c8784e3486c9afc992c3
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2024-09-20 16:23:21 +0900

    Title: 1차원 폭발 게임, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240920/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,39 @@
+n,m=map(int,input().split())
+bomb=[int(input()) for _ in range(n)]
+
+def boom(m):
+    pos=bomb[0]
+    cnt=1
+    sign=0
+    for i in range(1,len(bomb)):
+        if pos==bomb[i]:
+            cnt+=1
+        elif pos!=bomb[i] and cnt>=m:
+            sign=1
+            for j in range(cnt,0,-1):
+                bomb[i-j]=0
+        else:
+            pos=bomb[i]
+            cnt=1
+    if cnt>=m:
+        sign=1
+        for j in range(cnt,0,-1):
+            bomb[i-j]=0
+    return sign
+        
+
+def clean():
+    global bomb
+    temp=[]
+    for i in range(len(bomb)):
+        if bomb[i]:
+            temp.append(bomb[i])
+    bomb=temp[:]
+
+sign=1
+while sign and len(bomb)>0:
+    sign=boom(m)
+    clean()
+print(len(bomb))
+for i in range(len(bomb)):
+    print(bomb[i])
